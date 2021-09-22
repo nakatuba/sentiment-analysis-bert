90afb13081c2d8d0fd08439f4a0cb7ea78aae8e5
nakatuba <nakatuba0626@gmail.com>
2021-09-22 15:54:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# sentiment-analysis-bert
+
+1. Install python modules
+```
+poetry install
+```
+
+2. Download and make data
+```
+./make_data.sh
+```
---
+++ make_data.sh
@@ -0,0 +1,16 @@
+cd data
+
+# Download wrime dataset
+curl -O https://raw.githubusercontent.com/ids-cv/wrime/master/wrime.tsv
+# Fix column name
+if sed --version > /dev/null 2>&1; then
+  # Linux (GNU)
+  sed -i -e "1s/Saddness/Sadness/g" wrime.tsv
+else
+  # Mac (BSD)
+  sed -i "" -e "1s/Saddness/Sadness/g" wrime.tsv
+fi
+echo
+
+# Make labeled data
+python make_data.py
